12fb900e5cbe7ed139ecbfe7931a24dd1e2876eb
scukokot <demaao@mail.ru>
2018-12-05 14:18:14 +0200

    Add try exceptions to server IP and PORT, to have work in local and server invirenments

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 #!/home/c4850/tryrest.na4u.ru/.env/bin/python
 
 import os
+
+import catch as catch
 from flask import Flask
 
-IP = os.environ['APP_IP']
-PORT = int(os.environ['APP_PORT'])
+try:
+    IP = os.environ['APP_IP']
+    PORT = int(os.environ['APP_PORT'])
+except:
+    IP = '127.0.0.1'
+    PORT = '5000'
+
+
 
 app = Flask(__name__)
 
